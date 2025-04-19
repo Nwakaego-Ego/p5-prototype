e3f7e8f77166eea3cbcb882244e4dbc7638b654a
Ego Nwaekpe <glorynwaekpe@gmail.com>
2025-04-19 01:38:53 +0100

    Fixed Preview component

--- src/app/components/preview.jsx
+++ src/app/components/preview.jsx
@@ -1,18 +1,44 @@
+"use client";
+import dynamic from "next/dynamic";
+
+// Only load SketchPreview on the client
+const SketchPreview = dynamic(() => import("./SketchPreview"), { ssr: false });
+
 const Preview = ({ code, mode }) => {
   return (
     <div className="border p-4 mt-4">
       <h2 className="text-lg font-semibold mb-2">Preview:</h2>
+
       {mode === "iframe" ? (
         <iframe
           srcDoc={`<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/p5.min.js"></script><script>${code}</script>`}
           width="800"
           height="400"
-        ></iframe>
+        />
       ) : (
-        <div id="mySketch"></div>
+        <SketchPreview code={code} />
       )}
     </div>
   );
 };
 
 export default Preview;
+
+// const Preview = ({ code, mode }) => {
+//   return (
+//     <div className="border p-4 mt-4">
+//       <h2 className="text-lg font-semibold mb-2">Preview:</h2>
+//       {mode === "iframe" ? (
+//         <iframe
+//           srcDoc={`<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/p5.min.js"></script><script>${code}</script>`}
+//           width="800"
+//           height="400"
+//         ></iframe>
+//       ) : (
+//         <div id="mySketch"></div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Preview;
